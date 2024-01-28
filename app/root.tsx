96e6b3d8eca0ffa9a4db6241364dafe47f95de9d
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2024-01-28 12:19:17 +0530

    feat: add remix-themes

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,19 +7,41 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { themeSessionResolver } from "./utils/sessions.server";
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+};
+
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/resources/set-theme">
+      <App />
+    </ThemeProvider>
+  )
+}
+
+function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme()
+  return (
+    <html lang="en" data-theme={theme ?? ''}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
