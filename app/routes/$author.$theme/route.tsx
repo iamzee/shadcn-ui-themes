4d6fea0db8f523cdd6dfe6f9b3889bdae613e641
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2024-01-30 13:02:48 +0530

    feat: initial setup for action drawer

--- app/routes/$author.$theme/route.tsx
+++ app/routes/$author.$theme/route.tsx
@@ -7,6 +7,7 @@ import {
   // PageHeaderDescription,
   PageHeaderHeading,
 } from "./page-header";
+import { ActionDrawer } from "./action-drawer";
 
 export { loader } from "./loader.server";
 
@@ -16,43 +17,48 @@ export default function AuthorTheme() {
   return (
     // injecting theme css variables ---
     <div style={{ ...currTheme.light }}>
-      {/* TODO: add `container` to tailwind.config.js */}
-      <div className="container relative">
-        <PageHeader>
-          {/* <Announcement /> */}
-          <PageHeaderHeading className="hidden md:block">
-            Check out some examples
-          </PageHeaderHeading>
-          <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
-          {/* FIXME: react-wrap-balancer throwing some error */}
-          {/* <PageHeaderDescription>
-            Dashboard, cards, authentication. Some examples built using the
-            components. Use this as a guide to build your own.
-          </PageHeaderDescription> */}
-          <PageActions>
-            {/* <Link
-              href="/docs"
-              className={cn(buttonVariants(), "rounded-[6px]")}
-            >
-              Get Started
-            </Link>
-            <Link
-              href="/components"
-              className={cn(
-                buttonVariants({ variant: "outline" }),
-                "rounded-[6px]"
-              )}
-            >
-              Components
-            </Link> */}
-          </PageActions>
-        </PageHeader>
-        <section>
-          <ExamplesNav />
-          <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
-            <Outlet />
-          </div>
-        </section>
+      <div className="relative">
+        {/* TODO: add `container` to tailwind.config.js */}
+        <div className="container relative">
+          <PageHeader>
+            {/* <Announcement /> */}
+            <PageHeaderHeading className="hidden md:block">
+              Check out some examples
+            </PageHeaderHeading>
+            <PageHeaderHeading className="md:hidden">
+              Examples
+            </PageHeaderHeading>
+            {/* FIXME: react-wrap-balancer throwing some error */}
+            {/* <PageHeaderDescription>
+              Dashboard, cards, authentication. Some examples built using the
+              components. Use this as a guide to build your own.
+            </PageHeaderDescription> */}
+            <PageActions>
+              {/* <Link
+                href="/docs"
+                className={cn(buttonVariants(), "rounded-[6px]")}
+              >
+                Get Started
+              </Link>
+              <Link
+                href="/components"
+                className={cn(
+                  buttonVariants({ variant: "outline" }),
+                  "rounded-[6px]"
+                )}
+              >
+                Components
+              </Link> */}
+            </PageActions>
+          </PageHeader>
+          <section>
+            <ExamplesNav />
+            <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
+              <Outlet />
+            </div>
+          </section>
+        </div>
+        <ActionDrawer />
       </div>
     </div>
   );
