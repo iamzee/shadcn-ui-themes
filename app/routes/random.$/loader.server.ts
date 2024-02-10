1b57bb15296ade80dcfd83d0b304a77a45f7ec71
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2024-02-10 12:39:25 +0530

    refactor: generation of light and dark themes

--- app/routes/random.$/loader.server.ts
+++ app/routes/random.$/loader.server.ts
@@ -9,6 +9,28 @@ import { ZodError } from "zod";
 import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { paramsSchema } from "./params-schema";
 
+const shadcnVariableMaterialThemeMap = {
+  "--background": "background",
+  "--foreground": "onBackground",
+  "--card": "surface",
+  "--card-foreground": "onSurface",
+  "--popover": "surface",
+  "--popover-foreground": "onSurface",
+  "--primary": "primary",
+  "--primary-foreground": "onPrimary",
+  "--secondary": "secondary",
+  "--secondary-foreground": "onSecondary",
+  "--muted": "surfaceVariant",
+  "--muted-foreground": "outline",
+  "--accent": "surfaceVariant",
+  "--accent-foreground": "onSurfaceVariant",
+  "--destructive": "error",
+  "--destructive-foreground": "onError",
+  "--border": "outlineVariant",
+  "--input": "outlineVariant",
+  "--ring": "primary",
+};
+
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   try {
     // extract `sourceColor` and `example` from params (splat route)
@@ -19,165 +41,39 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 
     const theme = themeFromSourceColor(argbFromHex(sourceColor));
     const radius = new URL(request.url).searchParams.get("radius") || "0.5";
-    const light = {
-      "--background": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["background"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onBackground"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--card": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["surface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--card-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onSurface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--popover": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["surface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--popover-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onSurface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--primary": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["primary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--primary-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onPrimary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--secondary": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["secondary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--secondary-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onSecondary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--muted": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["surfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--muted-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["outline"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--accent": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["surfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--accent-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onSurfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--destructive": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["error"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--destructive-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["onError"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--border": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["outlineVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--input": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["outlineVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--ring": convert.hex
-        .hsl(hexFromArgb(theme.schemes.light["primary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--radius": `${radius}rem`,
-    };
+    const light = Object.keys(shadcnVariableMaterialThemeMap).reduce(
+      (prev, curr) => ({
+        ...prev,
+        [curr]: convert.hex
+          .hsl(
+            hexFromArgb(
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              theme.schemes.light[shadcnVariableMaterialThemeMap[curr]]
+            )
+          )
+          .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
+          .join(" "),
+      }),
+      { "--radius": `${radius}rem` }
+    );
 
-    const dark = {
-      "--background": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["background"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onBackground"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--card": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["surface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--card-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onSurface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--popover": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["surface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--popover-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onSurface"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--primary": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["primary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--primary-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onPrimary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--secondary": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["secondary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--secondary-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onSecondary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--muted": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["surfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--muted-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["outline"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--accent": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["surfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--accent-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onSurfaceVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--destructive": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["error"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--destructive-foreground": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["onError"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--border": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["outlineVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--input": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["outlineVariant"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--ring": convert.hex
-        .hsl(hexFromArgb(theme.schemes.dark["primary"]))
-        .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
-        .join(" "),
-      "--radius": `${radius}rem`,
-    };
+    const dark = Object.keys(shadcnVariableMaterialThemeMap).reduce(
+      (prev, curr) => ({
+        ...prev,
+        [curr]: convert.hex
+          .hsl(
+            hexFromArgb(
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              theme.schemes.dark[shadcnVariableMaterialThemeMap[curr]]
+            )
+          )
+          .map((v, i) => (i === 1 || i === 2 ? `${v}%` : v))
+          .join(" "),
+      }),
+      { "--radius": `${radius}rem` }
+    );
 
     return {
       light,
